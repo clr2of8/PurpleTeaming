3f4a8d75239a3e4ca6bb4fd3b48e47d475029bc3
Carrie Roberts <clr2of8@gmail.com>
2025-02-04 17:24:43 -0500

    Create Purple.psm1

---
+++ Tools/Purple/Purple.psm1
@@ -0,0 +1,39 @@
+function Purple-Redeploy {
+    IEX (IWR "https://raw.githubusercontent.com/clr2of8/PurpleTeaming/refs/heads/main/Tools/windows-vm-setup.ps1" -UseBasicParsing)
+}
+
+function Purple-InstallVSCode {
+    Write-Host "Installing VSCode" -ForegroundColor Cyan
+    Install-PackageProvider -Name NuGet -Force
+    Install-Script -Name Install-VSCode -Force
+    Install-VSCode
+}
+
+function Purple-PatchCaldera {
+    Install-Module -Name Posh-SSH -Force -ErrorAction Ignore
+    $password = ConvertTo-SecureString "metarange" -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential ("ubuntu", $password)
+    Get-SSHSession | Remove-SSHSession
+    $sess = New-SSHSession -ComputerName linux.cloudlab.lan -Credential $cred -Force
+    $commands = @(
+        "sudo kill -9 `$(sudo lsof -t -i :8888)",
+        "sed -i -r ""s/app.frontend.api_base_url: .*$/app.frontend.api_base_url: http:\/\/linux.cloudlab.lan:8888/g"" ~/caldera/conf/local.yml",
+        "sed -i -r ""s/app.contact.http: .*$/app.contact.http: http:\/\/linux.cloudlab.lan:8888/g"" ~/caldera/conf/local.yml",
+        "cd ~/caldera; .venv//bin//python3 server.py --build",
+        "sudo kill -9 `$(sudo lsof -t -i :8888)"
+    )
+    foreach ($command in $commands) {
+        invoke-sshcommand -SSHSession $sess -Command $command -ShowStandardOutputStream -ShowErrorOutputStream -TimeOut 300
+    }
+    $sess | Remove-SSHSession
+}
+
+function Purple-InstallAtomicRedTeam {
+
+}
+
+function Purple-InstallMACAT {
+
+}
+
+
