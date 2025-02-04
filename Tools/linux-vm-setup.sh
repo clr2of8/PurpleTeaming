dbf9d18738b3824385d2ec99355a5fd7f2a3e6f9
Carrie Roberts <clr2of8@gmail.com>
2025-02-04 16:50:26 -0500

    kill Caldera before patch

--- Tools/linux-vm-setup.sh
+++ Tools/linux-vm-setup.sh
@@ -20,9 +20,9 @@ cp conf/default.yml conf/local.yml
 sed -i -r "s/: admin.*$/: AtomicRedTeam1\!/g" conf/local.yml
 sed -i -r "s/admin: /art: /g" conf/local.yml
 # fix remote login bug https://github.com/mitre/caldera/issues/2901
+sudo kill -9 $(sudo lsof -t -i :8888)
 sed -i -r "s/app.frontend.api_base_url: .*$/app.frontend.api_base_url: http:\/\/linux.cloudlab.lan:8888/g" conf/local.yml
 sed -i -r "s/app.contact.http: .*$/app.contact.http: http:\/\/linux.cloudlab.lan:8888/g" conf/local.yml
-sudo kill -9 $(sudo lsof -t -i :8888)
 .venv/bin/python3 server.py --build &
 
 echo "****Installing VECTR****"
