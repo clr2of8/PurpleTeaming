774f6a1c2a22897d3cbbccef66a41a667da8747f
Carrie Roberts <clr2of8@example.com>
2025-10-03 21:58:27 -0600

    remove patch caldera

--- Tools/Purple/Purple.psm1
+++ Tools/Purple/Purple.psm1
@@ -9,25 +9,6 @@ function Purple-InstallVSCode {
     Install-VSCode
 }
 
-function Purple-PatchCaldera {
-    Install-Module -Name Posh-SSH -Force -ErrorAction Ignore
-    $password = ConvertTo-SecureString "metarange" -AsPlainText -Force
-    $cred = New-Object System.Management.Automation.PSCredential ("ubuntu", $password)
-    Get-SSHSession | Remove-SSHSession
-    $sess = New-SSHSession -ComputerName linux.cloudlab.lan -Credential $cred -Force
-    $commands = @(
-        "sudo kill -9 `$(sudo lsof -t -i :8888)",
-        "sed -i -r ""s/app.frontend.api_base_url: .*$/app.frontend.api_base_url: http:\/\/linux.cloudlab.lan:8888/g"" ~/caldera/conf/local.yml",
-        "sed -i -r ""s/app.contact.http: .*$/app.contact.http: http:\/\/linux.cloudlab.lan:8888/g"" ~/caldera/conf/local.yml",
-        "cd ~/caldera; .venv//bin//python3 server.py --build",
-        "sudo kill -9 `$(sudo lsof -t -i :8888)"
-    )
-    foreach ($command in $commands) {
-        invoke-sshcommand -SSHSession $sess -Command $command -ShowStandardOutputStream -ShowErrorOutputStream -TimeOut 300
-    }
-    $sess | Remove-SSHSession
-}
-
 function Purple-InstallAtomicRedTeam {
     Add-MpPreference -ExclusionPath C:\AtomicRedTeam\
     IEX (IWR 'https://raw.githubusercontent.com/redcanaryco/invoke-atomicredteam/master/install-atomicredteam.ps1' -UseBasicParsing);
